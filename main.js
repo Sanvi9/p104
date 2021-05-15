fa5a4a4b7ad9d7a02af13123d4b25c54ff2bae9c
Sanvi9 <74234358+Sanvi9@users.noreply.github.com>
2021-05-15 15:27:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
-Webcam.set(
-    {
-        width:300,
-        height:300,
-        image_format:'png',
-        png_quality:90,
-  });
-  camera = document.getElementById("camera");
-  Webcam.attach('#camera')
-  function take_snapshot() {
-      Webcam.snap(function(data_url){
-          document.getElementById("result").innerHTML = "<img id='captured_image' src='" + data_url + "'>"
-      });
-  }
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quailty:90
+});
 
-  console.log("ml5 version",ml5.version);
-  classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/j4D5H9TUl/model.json",modelLoaded)
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured" src="'+data_uri+'"/>"';
+    });
+}
+
+console.log('ml5 version: ',ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/GFhhHoc5x/model.json",modelLoaded)
+
+function modelLoaded() {
+    console.log('Model Loaded')
+}
